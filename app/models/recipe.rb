c1fec364c9b6767ecc4097ffcb8498b3cdc0035c
Roman Ventskus <scofield665@gmail.com>
2015-05-18 07:50:50 +0300

    Calories and etc added to view of recipe

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,5 @@ class Recipe < ActiveRecord::Base
   has_many :products, :through => :ingredients
   validates :name, presence: { message: "не может быть пустым" }
   accepts_nested_attributes_for :ingredients, :reject_if => :all_blank, :allow_destroy => true
+  attr_accessor :weight
 end
